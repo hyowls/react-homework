3c1cec3a4d4b085c37594eca6e8adaab7d5667bd
hyowls <hhvucyh@gmail.com>
2024-05-29 17:24:18 +0900

    counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './App.css'
 const App = () => {
+  const [count, setCount] = useState(0);
+  const plusCount = () => {
+    setCount(count + 1)
+  }
+  const minusCount = () => {
+    setCount(count - 1)
+  }
   return (
-    <div>App</div>
+    <div className='wrap'>
+      <p>현재 카운트 : {count}</p>
+      <div>
+        <button className='btn' onClick={plusCount}> + </button>
+        <button className='btn' onClick={minusCount}> - </button>
+      </div>
+    </div>
   )
 }
 
